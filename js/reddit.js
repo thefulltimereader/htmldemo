f1ec31a0f4fdc1200e83b4515b997807f694dfdb
thefulltimereader <ajk377@box259.cims.nyu.edu>
2011-02-14 22:43:45 -0500

    json works

--- js/reddit.js
+++ js/reddit.js
@@ -1,38 +1,31 @@
 //fetches and decomposes json feed from reddit pics
 
+/**
+ * using jsonp to go around cross-domain issues
+ */
 $.ajax({
-  url:'http://www.reddit.com/r/pics/.json?limit=20&jsonp=alert',
+  url:'http://www.reddit.com/r/pics/.json?limit=20&jsonp=?',
   dataType:'jsonp',
   success:function(json){
-    alert(json.data);
-    $.each(json.data.children, function(){
-    alert("sdf"+this.data.title);
-	     alert(this.data.title);
-});
-  },
-  error: function(xhr,status, errorThrown){
-    console.log("Reddit didn't respond! : " + status);
-}
-});
-
-/*
- 
-$.ajax({
-  url:'http://www.reddit.com/r/pics/.json?limit=20',
-  dataType:'json',
-  success:function(json){
-    alert(json.data);
-    $.each(json.data.children, function(){
-    alert("sdf"+this.data.title);
-	     alert(this.data.title);
-});
-  },
-  error: function(xhr,status, errorThrown){
-    console.log("Reddit didn't respond! : " + status);
-}
-});
-
+//want to recreate:
+/**
+ * <li><a href="http://i.imgur.com/18U4t.jpg" ref="reddit" title="Regarding, 'There Are No Rules.'">Regarding, 'There Are No Rules.'</a></li>
  */
+i=0;
+alert(json.data.children[1].title);
+    $.each(json.data.children, function(i, data){
+	     title = data.title;
+	if(i==0){ alert(title);alert(this.url); alert(data.kind);i++;}
+	     url = data.url;
+	     output = "<li><a href='" +url + "' ref='reddit' title='"+title+"'>"+title+"</a></li>";
+	     $('#gallery ul').append(output);
+});
+  },
+  error: function(xhr,status, errorThrown){
+    console.log("Reddit didn't respond! : " + errorThrown);
+}
+});
+
 
 
 /*** .ajax function is equivalent to
